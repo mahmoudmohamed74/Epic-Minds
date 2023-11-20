c2c51608b9dc5073bb27669ab7770784a07cf45a
Mahmoud <mahmoudmohamed74>
2023-11-20 16:55:43 +0200

    fetchBestSellerBooks repo impl

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -2,12 +2,27 @@ import 'package:epic_minds/Features/home/data/models/book_model/book_model.dart'
 import 'package:dartz/dartz.dart';
 import 'package:epic_minds/Features/home/data/repos/home_repo.dart';
 import 'package:epic_minds/core/errors/failure.dart';
+import 'package:epic_minds/core/utils/api_service.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() async {
+    try {
+      var data = await apiService.get(
+        endPoint: "volumes?Filtering=free-ebooks&Soring=newest&q=harry potter",
+      );
+
+      List<BookModel> books = [];
+      for (var item in data['item']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } on Exception catch (e) {
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
