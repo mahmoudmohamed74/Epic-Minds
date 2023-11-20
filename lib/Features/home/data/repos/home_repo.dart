27b22228356f533e057e3c05176cfd6e12af5b28
Mahmoud <mahmoudmohamed74>
2023-11-20 15:21:36 +0200

    home repo / dartZ

---
+++ lib/Features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:epic_minds/Features/home/data/models/book_model/book_model.dart';
+import 'package:epic_minds/core/errors/failure.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+}
